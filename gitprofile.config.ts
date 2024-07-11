7184e12e371b95316753aeb37ece83e254e6e244
Steven <stevenrim@gmail.com>
2024-07-10 21:16:15 -0400

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -82,10 +82,6 @@ const CONFIG = {
     fileUrl:
       'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
-  skills: [
-    '',
-    
-  ],
   experiences: [
     {
       company: '',
