796511c7f375bc079ac05d111100a07a5bcf7bf7
Steven <stevenrim@gmail.com>
2024-07-11 19:38:11 -0400

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -29,7 +29,7 @@ const CONFIG = {
       },
     },
     external: {
-      header: 'My Projects',
+      header: 'My Projiuyiyiects',
       // To hide the `External Projects` section, keep it empty.
       projects: [
         {
